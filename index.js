41d26b88eccac3c6bf58fdadcf578c033f26e4e9
lishu <lishugupta652@gmail.com>
2021-11-16 18:21:40 +0530

    terminal

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 3000;
-
+const mongoose = require("mongoose");
+const env = require("dotenv").config();
 // Import routes
 const authRoutes = require("./routes/auth");
 
+// Connnect to Database using mongoose
+mongoose.connect(process.env.MONGODB_URI, () => {
+  console.log("Connected to Db");
+});
+
+// MiddleWare to use json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/app/user", authRoutes);
+// Index Route
 app.get("/", (req, res) => {
   res.send({
     msg: "Hello there this app is working totally fine",
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,22 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
 router.get("/register", (req, res) => {
   console.log("Reached Route");
   res.send("try to send a post request");
 });
-router.post("/register", (req, res) => {
-  console.log("Registered");
-  res.send("Registered");
+router.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch {
+    res.status(404).send("Error");
+  }
 });
 
 module.exports = router;
