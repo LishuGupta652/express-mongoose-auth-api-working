c51a679dd9a9aadb42f2a59fb3fb74e32a9e21da
lishu <lishugupta652@gmail.com>
2021-11-16 20:16:04 +0530

    terminal

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const verify = require("./verifyToken");
+const User = require("../models/User");
 router.get("/", verify, (req, res) => {
+  //   res.send(req.user);
+  //   User.findbyOne({ _id: req.user });
   res.json({ posts: [{ title: "My first post", descripton: "Randon data" }] });
 });
 
